a4ce756a005db4081183477c31e8b639388df244
Oliver Kötter <oliver.koetter@tns-infratest.com>
2014-09-03 09:31:06 +0200

    altap-salamander-beta 3.0.2

---
+++ altap-salamander-beta/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'altap-salamander-beta'
+$installerType = 'exe'
+$url = 'http://www.altap.cz/ftp/salamand/as302x86.exe'
+$url64bit = 'http://www.altap.cz/ftp/salamand/as302x64.exe'
+$silentArgs = '/x /s'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64bit" -validExitCodes $validExitCodes
