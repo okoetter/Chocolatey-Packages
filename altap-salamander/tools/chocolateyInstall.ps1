238140368a7a7b7ffb2e3c08385af533f29c290e
Oliver Kötter <oliver@koetter.cc>
2014-09-03 09:47:28 +0200

    altap-salamander 3.0.3

--- altap-salamander/tools/chocolateyInstall.ps1
+++ altap-salamander/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'altap-salamander'
 $installerType = 'exe'
-$url = 'http://www.altap.cz/ftp/salamand/as302x86.exe'
-$url64bit = 'http://www.altap.cz/ftp/salamand/as302x64.exe'
+$url = 'http://www.altap.cz/ftp/salamand/as303x86.exe'
+$url64bit = 'http://www.altap.cz/ftp/salamand/as303x64.exe'
 $silentArgs = '/x /s'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64bit" -validExitCodes $validExitCodes
