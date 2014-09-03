190bbf33de11904f4682eb3d8a8af64a781883a1
Oliver Kötter <oliver@koetter.cc>
2014-09-03 09:48:26 +0200

    mediamonkey 4.1.4

--- mediamonkey/tools/chocolateyInstall.ps1
+++ mediamonkey/tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'mediamonkey'
 $installerType = 'exe'
-#$url = 'http://www.mediamonkey.com/MediaMonkey_Setup.exe'
-$url = 'http://4258.voxcdn.com/sw/MediaMonkey_4.1.3.1708.exe'
+$url = 'http://www.mediamonkey.com/MediaMonkey_Setup.exe'
 $silentArgs = '/verysilent'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
