710bfef354201e1a8dca3fb47241f5d8126af645
Oliver Kötter <oliver@koetter.cc>
2015-03-10 09:01:33 +0100

    use version-specific download url for mediamonkey

--- mediamonkey/tools/chocolateyInstall.ps1
+++ mediamonkey/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'mediamonkey'
 $installerType = 'exe'
-$url = 'http://www.mediamonkey.com/MediaMonkey_Setup.exe'
+$url = 'http://e8d327b5fb.site.internapcdn.net/sw/MediaMonkey_4.1.6.1736.exe'
 $silentArgs = '/verysilent'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
