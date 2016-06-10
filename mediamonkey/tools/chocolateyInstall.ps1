b2bffdc1d9303a2b148d3acf9c637d84201a41a2
Oliver Kötter <oliver@koetter.cc>
2016-06-10 08:47:01 +0200

    mediamonkey 4.1.12

--- mediamonkey/tools/chocolateyInstall.ps1
+++ mediamonkey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageName= 'mediamonkey'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://e8d327b5fb.site.internapcdn.net/sw/MediaMonkey_4.1.11.1783.exe'
+$url        = 'http://e8d327b5fb.site.internapcdn.net/sw/MediaMonkey_4.1.12.1798.exe'
 $url64      = ''
 $packageArgs = @{
   packageName   = $packageName
