7ad386f626f7d7089ba54d8718247f1e501c5db8
Oliver Kötter <oliver@koetter.cc>
2016-06-29 10:47:37 +0200

    altap-salamander 3.0.7

--- altap-salamander/tools/chocolateyInstall.ps1
+++ altap-salamander/tools/chocolateyInstall.ps1
@@ -1,7 +1,21 @@
-﻿$packageName = 'altap-salamander'
-$installerType = 'exe'
-$url = 'http://www.altap.cz/ftp/salamand/as306x86.exe'
-$url64bit = 'http://www.altap.cz/ftp/salamand/as306x64.exe'
-$silentArgs = '/x /s'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64bit" -validExitCodes $validExitCodes
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageName= 'altap-salamander' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.altap.cz/ftp/salamand/as307x86.exe' # download url
+$url64      = 'http://www.altap.cz/ftp/salamand/as307x64.exe' # 64bit URL here or remove - if installer is both, use $url
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+  silentArgs = '/x /s'
+  validExitCodes = @(0)
+  softwareName  = 'altap-salamander*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = 'DDD012C700CFABE67F9538349843A56C'
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = 'E3F5B1060BF944C07B110B55A029777C'
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
--- altap-salamander/tools/chocolateyUninstall.ps1
+++ altap-salamander/tools/chocolateyUninstall.ps1
@@ -3,19 +3,21 @@ try {
     $fileType = "exe"
     $silentArgs = "/x /s"
     $validExitCodes = @(0)
-    
+
     $unfile = "${Env:ProgramFiles}\Altap Salamander\remove\remove.exe"
     $unfilex86 = "${Env:ProgramFiles(x86)}\Altap Salamander\remove\remove.exe"
 
     if (Test-Path "$unfilex86") {$file = "$unfilex86"}
     if (Test-Path "$unfile") {$file = "$unfile"}
-        
+
     if ((Test-Path "$unfile") -or (Test-Path "$unfilex86")) {
-        Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file -validExitCodes $validExitCodes
+        Uninstall-ChocolateyPackage -PackageName $packageName
+                                    -FileType $fileType
+                                    -SilentArgs $silentArgs
+                                    -ValidExitCodes $validExitCodes
+                                    -File $file
     }
-    
-    Write-ChocolateySuccess $packageName
 } catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    Write-Warning "Could not uninstall"
     throw
 }
