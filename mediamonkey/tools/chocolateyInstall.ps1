26a43b6b31367f79e7d171aa263e48127045702b
Oliver Kötter <oliver@koetter.cc>
2017-09-29 14:30:08 +0200

    mediamonkey 4.1.18

--- mediamonkey/tools/chocolateyInstall.ps1
+++ mediamonkey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageName= 'mediamonkey'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://e8d327b5fb.site.internapcdn.net/sw/MediaMonkey_4.1.17.1840.exe'
+$url        = 'http://e8d327b5fb.site.internapcdn.net/sw/MediaMonkey_4.1.18.1852.exe'
 $url64      = ''
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +12,7 @@ $packageArgs = @{
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'mediamonkey*'
-  checksum      = '72ebe2e68363999fdfba17cfc5c13dfe8b37794d77b80624cb54e688033650d2079503a457f210a42903379c9d45f9c1bcb2aaa01c8f5919740770cd1f337ddd'
+  checksum      = '90695e989b68e59482409baad246920dbd6e508e4bc6152bff0f79f4d6b56507229d21995d55be4858f844c1504aac7f4c54a84e8049fadcb71abdd918294f99'
   checksumType  = 'sha512'
   checksum64    = ''
   checksumType64= 'sha512'
